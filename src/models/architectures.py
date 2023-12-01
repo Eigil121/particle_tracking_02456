ab5e196b9378b5520834106e31f594da137ec132
andr917d <farimag@live.dk>
2023-12-01 12:11:09 +0100

    Added an improved CNN

--- src/models/architectures.py
+++ src/models/architectures.py
@@ -14,3 +14,27 @@ class SimpleCNN(nn.Module):
         x = torch.relu(self.conv2(x))
         x = self.activation(self.conv3(x))
         return x
+    
+
+class ImprovedCNN(nn.Module):
+    def __init__(self):
+        super(ImprovedCNN, self).__init__()
+        self.conv1 = nn.Conv2d(5, 64, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(128)
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        self.bn3 = nn.BatchNorm2d(256)
+        self.conv4 = nn.Conv2d(256, 1, kernel_size=3, padding=1)
+        self.activation = nn.Sigmoid()
+        self.dropout = nn.Dropout(0.25)
+
+    def forward(self, x):
+        x = torch.relu(self.bn1(self.conv1(x)))
+        x = self.dropout(x)
+        x = torch.relu(self.bn2(self.conv2(x)))
+        x = self.dropout(x)
+        x = torch.relu(self.bn3(self.conv3(x)))
+        x = self.dropout(x)
+        x = self.activation(self.conv4(x))
+        return x
